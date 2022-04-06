c0ebb1fa786086ba827280db41f27110d4bb6c80
v.georgy86@gmail.com <Vjnjh8688>
2022-04-06 12:13:13 +0300

    изменена ссылка

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const baseURL = "http://192.168.1.178:8764";
-// const baseURL = "https://eam.interid.ru:8282";
+// const baseURL = "http://192.168.1.178:8764"
+// const baseURL = "http://eam.interid.ru:8282";
+// const baseURL = "http://localhost:8282";
 // const baseURL = "http://192.168.1.95:8765";
+// const baseURL = "https://localhost:8282";
+const baseURL = "http://9090.union-eam.ru:8282";
 //
 export async function auth(data, unionUrl) {
   let dataToSend;
@@ -33,6 +36,7 @@ export async function getSettings() {
     .get(`${baseURL}/api/settings/`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -46,6 +50,7 @@ export function postSettings(settings) {
     .post(`${baseURL}/api/settings/`, settings, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -59,6 +64,7 @@ export async function getLocations(location, params) {
     .get(`${baseURL}/api/${location}/`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
       params,
     })
@@ -74,6 +80,7 @@ export async function getReaders() {
     .get(`${baseURL}/api/readers/`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -88,6 +95,7 @@ export async function getAntennas(readerName) {
     .get(`${baseURL}/api/antennas?readerName=${readerName}`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -114,6 +122,7 @@ export async function saveDevice(newRow, headerTable) {
     .post(`${baseURL}/api/${path}`, newRow, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -129,6 +138,7 @@ export async function getOrganizations() {
     .get(`${baseURL}/api/organizations`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -143,6 +153,7 @@ export async function getResponsible() {
     .get(`${baseURL}/api/system-responsible`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Access-Control-Allow-Origin": "*",
       },
     })
     .then((response) => {
@@ -153,7 +164,12 @@ export async function getResponsible() {
 
 export async function getBaseUrlFromProp() {
   let data;
-  await axios.get(`${baseURL}/api/unionUrl/`, {}).then((response) => {
+  await axios.get(`${baseURL}/api/unionUrl/`, {
+    headers: {
+
+      "Access-Control-Allow-Origin": "*",
+    },
+  }).then((response) => {
     data = response.data;
   });
   return data;
