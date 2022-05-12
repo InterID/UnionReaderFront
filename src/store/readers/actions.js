737975ca18769716eac4912e03b755dd0c952bea
Fedor <plechov.fedor@gmail.com>
2022-05-12 17:39:53 +0300

    chore: delete console

--- src/store/readers/actions.js
+++ src/store/readers/actions.js
@@ -17,26 +17,21 @@ const responseHandler = (data, dispatch, isFetchReaders = false) => {
 
 export default {
   async fetchReaders({commit, dispatch}) {
-    console.log('fetching...');
     const result = await getAllReaders();
     responseHandler(result.data, dispatch, true)
-    console.log(result);
     commit("SET_READERS", result.data.value);
   },
   async connectReader({dispatch}, {api, port}) {
     const result = await connectNewReader(api, port);
     responseHandler(result.data, dispatch)
-    console.log(result);
   },
   async startInventory({dispatch}, {api, port}) {
     const result = await startInventory(api, port);
     responseHandler(result.data, dispatch)
-    console.log(result)
   },
   async stopInventory({dispatch}, {api, port}) {
     const result = await stopInventory(api, port);
     responseHandler(result.data, dispatch)
-    console.log(result)
   },
   setResponseMessage({commit, dispatch}, payload) {
     commit("SET_RESPONSE_MESSAGE", payload);// payload = {isError: Boolean, message: String}
