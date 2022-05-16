2918d0d49187e1f4738128cc1b9b326f1d31a590
Fedor <plechov.fedor@gmail.com>
2022-05-16 12:02:04 +0300

    feat: add endpoints for Reader

--- src/api/readers-api.js
+++ src/api/readers-api.js
@@ -7,47 +7,46 @@ readersControl.interceptors.response.use(function (response) {
   // Do something with response data
   return response;
 }, function (error) {
-  getStore().dispatch("readers/setResponseMessage", { isError: true, message: error.message });
+  getStore().dispatch("readers/setResponseMessage", {isError: true, message: error.message});
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
   return Promise.reject(error);
 });
 
 export async function getAllReaders() {
-  const response = await readersControl.get('get/readers/')
+  const response = await readersControl.get('')
   return response;
 }
 
 export async function connectNewReader(ip, port) {
-  const response = await readersControl.post('connect', {
+  const response = await readersControl.post('', {
     ip,
     port
   })
   return response;
 }
-export async function editReader(ip, port) {
-  const response = await readersControl.post('connect', {
-    ip,
-    port
-  }) // need add prev value of ip and port readers
+
+export async function deleteReader(ip, port) {
+  const response = await readersControl.delete(`ip/${ip}/port/${port}`);
+  return response;
+}
+
+export async function connectReader(ip, port) {
+  const response = await readersControl.post(`ip/${ip}/port/${port}/connect`);
+  return response;
+}
+
+export async function disconnectReader(ip, port) {
+  const response = await readersControl.post(`ip/${ip}/port/${port}/disconnect`);
   return response;
 }
 
 export async function startInventory(ip, port) {
-  const response = await readersControl.post('start/simple-inventory',
-    {
-      ip,
-      port
-    }
-  )
+  const response = await readersControl.post(`ip/${ip}/port/${port}/start-inventory`);
   return response;
 }
+
 export async function stopInventory(ip, port) {
-  const response = await readersControl.post('stop',
-    {
-      ip,
-      port
-    }
-  )
+  const response = await readersControl.post(`ip/${ip}/port/${port}/stop-inventory`);
   return response;
 }
